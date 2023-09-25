d9639b4a30bff98a6255bba61e3a1d1a81fb9a1e
SuhaibRumi <84827527+SuhaibRumi@users.noreply.github.com>
2023-09-25 15:22:30 +0500

    sometihng fix

--- bin/functions/example.dart
+++ bin/functions/example.dart
@@ -11,6 +11,23 @@ printEvenNumbers() {
   print(sum);
 }
 
+sumNum() {
+  List num = [10, 20, 35, 40, 55, 60, 75, 80, 95, 120];
+  var sum = 0;
+  var reminder = 0;
+  var largeNum = 0;
+
+  for (int i = 0; i < num.length; i++) {
+    sum = num[i] + sum;
+    largeNum = num[i];
+    reminder = sum % largeNum;
+  }
+  print("Sum is = $sum");
+  print("largeNum is = $largeNum");
+  print("reminder is = $reminder");
+}
+
 void main(List<String> args) {
-  printEvenNumbers();
+  // printEvenNumbers();
+  sumNum();
 }
